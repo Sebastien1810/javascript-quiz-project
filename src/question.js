03c2666e5143dc314989a45a9d154e5c07545c59
Jihan Shamas <jihan.shamas@gmail.com>
2025-01-20 18:00:40 +0100

    first day quiz

--- src/question.js
+++ src/question.js
@@ -1,7 +1,27 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (text, choices, answer, difficulty)
 
-    // 2. shuffleChoices()
+  // 2. shuffleChoices()
+
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
+  shuffleChoices() {
+    for (let i = this.choices.length - 1; i > 0; i--) {
+      // Generate a random index between 0 and i
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      // Swap elements at index i and randomIndex
+      [this.choices[i], this.choices[randomIndex]] = [
+        this.choices[randomIndex],
+        this.choices[i],
+      ];
+    }
+    return this.choices;
+  }
 }
+const result = new Question();
